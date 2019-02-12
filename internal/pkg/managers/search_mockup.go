590e81861aa8e441f29bc83df086b783dee18f6b
Giel de Nijs <giel@daisho.group>
2019-02-12 12:35:06 +0100

    [NP-617] Add handler tests.

---
+++ internal/pkg/managers/search_mockup.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019 Nalej - All Rights Reserved
+ */
+
+package managers
+
+import (
+	"context"
+
+	"github.com/nalej/derrors"
+
+        grpc "github.com/nalej/grpc-unified-logging-go"
+)
+
+type MockupSearchManager struct {
+}
+
+func NewMockupSearchManager() *MockupSearchManager {
+	return &MockupSearchManager{}
+}
+
+func (m *MockupSearchManager) Search(ctx context.Context, request *grpc.SearchRequest) (*grpc.LogResponse, derrors.Error) {
+        response := &grpc.LogResponse{
+                OrganizationId: request.GetOrganizationId(),
+                AppInstanceId: request.GetAppInstanceId(),
+                From: request.GetFrom(),
+                To: request.GetTo(),
+                Entries: []*grpc.LogEntry{},
+        }
+        return response, nil
+}
