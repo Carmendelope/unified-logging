bed030937cadae191f8007a54260fb3fb3f89b69
Giel de Nijs <giel@daisho.group>
2019-04-04 08:55:46 -0700

    [NP-1096] Remove namespace filter.
    deployment-manager now adds number of retries to namespace and that is
    information we do not have in unified-logging. Remove namespace filter
    to fix this.
    
    Also updated all vendors.

--- pkg/entities/fields.go
+++ pkg/entities/fields.go
@@ -8,7 +8,6 @@ package entities
 
 import (
 	"github.com/nalej/deployment-manager/pkg/utils"
-	"github.com/nalej/deployment-manager/pkg/common"
 )
 
 type Field string
@@ -17,7 +16,6 @@ type Field string
 // https://www.elastic.co/guide/en/beats/filebeat/current/add-kubernetes-metadata.html
 // https://www.elastic.co/blog/shipping-kubernetes-logs-to-elasticsearch-with-filebeat
 
-// Should we get these from deployment-manager/pkg/utils?
 const (
         TimestampField Field = "@timestamp"
 	NamespaceField Field = "kubernetes.namespace"
@@ -39,11 +37,6 @@ type FilterFields struct {
 
 func (f *FilterFields) ToFilters() SearchFilter {
 	filters := make(SearchFilter)
-
-	// Is this needed? Does this speed up or slow down?
-	if f.OrganizationId != "" && f.AppInstanceId != "" {
-		filters[NamespaceField] = []string{common.GetNamespace(f.OrganizationId, f.AppInstanceId)}
-	}
 
 	if f.OrganizationId != "" {
 		filters[OrganizationIdField] = []string{f.OrganizationId}
